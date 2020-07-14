0bc8ba8d34b637b221cccf4d6a06e12f967339ff
Kodanda Ramu Kakarla <kkakarla@kkakarla.pnq.csb>
2020-07-14 16:31:58 +0530

    bean bindings

---
+++ Camel-Core/camel-call-java-main-class-method-in-another-bundle/src/main/java/com/test/TestUtil.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUtil {
+      
+        public String[] getArgs()
+        {
+            String[]  args = {"100","200"};
+            
+            return args;
+        }
+     }
+
+
